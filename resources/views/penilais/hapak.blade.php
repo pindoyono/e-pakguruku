074198ad785428357cbd70bd1dd63a1fc369f858
pindoyono <pindoyonopatan@gmail.com>
2023-05-30 19:45:45 +0800

    user

--- resources/views/penilais/hapak.blade.php
+++ resources/views/penilais/hapak.blade.php
@@ -606,8 +606,8 @@
             <tr style="font-weight: 900">
                 <td>Masa Kerja Golongan -</td>
                 <td
-                    style=" {{ masa_kerja_tahun(\Carbon\Carbon::parse(date('Y') . '-' . get_periode() . '-1'), Auth::user()->tmt_pns) >= 2 ? 'color: green;' : 'color: red;' }}">
-                    {{ masa_kerja(\Carbon\Carbon::parse(date('Y') . '-' . get_periode() . '-1'), Auth::user()->tmt_pns) }}
+                    style=" {{ masa_kerja_tahun(\Carbon\Carbon::parse(date('Y') . '-' . get_periode() . '-1'), $user->tmt_pns) >= 2 ? 'color: green;' : 'color: red;' }}">
+                    {{ masa_kerja(\Carbon\Carbon::parse(date('Y') . '-' . get_periode() . '-1'), $user->tmt_pns) }}
                 </td>
 
                 <td colspan="4"> TMT pangkat Terakhir ( {{ tgl_indo($user->tmt_pns) }} )</td>
