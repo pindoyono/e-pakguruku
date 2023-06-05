7882960f2ae69b98e3fa8a0539695b8607140a92
pindoyono <pindoyonopatan@gmail.com>
2023-06-05 11:40:31 +0800

    test

--- app/Http/Controllers/PenilaiController.php
+++ app/Http/Controllers/PenilaiController.php
@@ -811,7 +811,7 @@ class PenilaiController extends Controller
     {
         if (Auth::user()->hasRole('super-admin')) {
             $data = DB::table('paks')
-                ->select('paks.*', 'paks.id as pak_id', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
+                ->select('paks.*', 'paks.id as pak_id', 'paks.created_at as tgl_buat', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
                     'users.ijazah_tidak_sesuai as ijazah_tidak_sesuai_user', 'users.pendukung_tugas_guru as pendukung_tugas_guru_user', 'users.memperoleh_penghargaan as memperoleh_penghargaan_user',
                     'users.tmt_pns', 'users.karya_inovatif as ki_user', 'users.publikasi_ilmiah as pi_user', 'jabatans.*')
                 ->join('users', 'users.id', '=', 'paks.user_id')
@@ -822,7 +822,7 @@ class PenilaiController extends Controller
                 ->get();
 
             $pak2 = DB::table('paks')
-                ->select('paks.*', 'paks.id as pak_id', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
+                ->select('paks.*', 'paks.id as pak_id', 'paks.created_at as tgl_buat', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
                     'users.ijazah_tidak_sesuai as ijazah_tidak_sesuai_user', 'users.pendukung_tugas_guru as pendukung_tugas_guru_user', 'users.memperoleh_penghargaan as memperoleh_penghargaan_user',
                     'users.tmt_pns', 'users.karya_inovatif as ki_user', 'users.publikasi_ilmiah as pi_user', 'jabatans.*')
                 ->join('users', 'users.id', '=', 'paks.user_id')
@@ -835,7 +835,7 @@ class PenilaiController extends Controller
 
         } else {
             $data = DB::table('paks')
-                ->select('paks.*', 'paks.id as pak_id', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
+                ->select('paks.*', 'paks.id as pak_id', 'paks.created_at as tgl_buat', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
                     'users.ijazah_tidak_sesuai as ijazah_tidak_sesuai_user', 'users.pendukung_tugas_guru as pendukung_tugas_guru_user', 'users.memperoleh_penghargaan as memperoleh_penghargaan_user',
                     'users.tmt_pns', 'users.karya_inovatif as ki_user', 'users.publikasi_ilmiah as pi_user', 'jabatans.*')
                 ->join('users', 'users.id', '=', 'paks.user_id')
@@ -848,7 +848,7 @@ class PenilaiController extends Controller
                 ->get();
 
             $pak2 = DB::table('paks')
-                ->select('paks.*', 'paks.id as pak_id', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
+                ->select('paks.*', 'paks.id as pak_id', 'paks.created_at as tgl_buat', 'users.name', 'users.username', 'users.sekolah', 'users.no_hp', 'users.tmt_cpns', 'tmt_jabatan', 'sekolah', 'users.username as nip', 'users.pendidikan', 'jabatans.id as jabatan_id', 'users.pangkat_golongan', 'users.tertinggal as tertinggal_user', 'users.pengembangan_diri as pd_user',
                     'users.ijazah_tidak_sesuai as ijazah_tidak_sesuai_user', 'users.pendukung_tugas_guru as pendukung_tugas_guru_user', 'users.memperoleh_penghargaan as memperoleh_penghargaan_user',
                     'users.tmt_pns', 'users.karya_inovatif as ki_user', 'users.publikasi_ilmiah as pi_user', 'jabatans.*')
                 ->join('users', 'users.id', '=', 'paks.user_id')
